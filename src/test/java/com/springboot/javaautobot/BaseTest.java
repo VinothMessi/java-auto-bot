dbaec61db2163158ad2d263b0952e84e25019414
vinothmessi <vinothone2one@gmail.com>
2021-08-05 22:42:17 +0530

    Added Initial dependencies

---
+++ src/test/java/com/springboot/javaautobot/BaseTest.java
@@ -0,0 +1,9 @@
+package com.springboot.javaautobot;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+
+@SpringBootTest
+public class BaseTest extends AbstractTestNGSpringContextTests {
+
+}
---
+++ src/test/java/com/springboot/javaautobot/tests/VisaRegistration.java
@@ -0,0 +1,11 @@
+package com.springboot.javaautobot.tests;
+
+import com.springboot.javaautobot.BaseTest;
+import org.testng.annotations.Test;
+
+public class VisaRegistration extends BaseTest {
+    @Test
+    public void visaRegistration() {
+        System.out.println("Visa Registration Test");
+    }
+}
