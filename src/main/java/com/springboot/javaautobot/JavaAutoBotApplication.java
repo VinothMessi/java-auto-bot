3a86cbc54e1af6b7d4979e515ce5cbb5a702ab99
vinothmessi <vinothone2one@gmail.com>
2021-08-05 21:01:28 +0530

    Initial commit

---
+++ src/main/java/com/springboot/javaautobot/JavaAutoBotApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.javaautobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAutoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAutoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/javaautobot/JavaAutoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.javaautobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAutoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
